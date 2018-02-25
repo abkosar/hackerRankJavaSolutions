c85f386586bd76b54eb831a38913a36b07dce9c3
Arda Kosar <ardkosar@usnycpww9401.global.publicisgroupe.net>
2018-02-25 01:35:06 -0500

    Fixing -1 bug

--- data_types/src/data_types/Solution.java
+++ data_types/src/data_types/Solution.java
@@ -21,9 +21,9 @@ class Solution{
                 long x=sc.nextLong();
                 System.out.println(x+" can be fitted in:");
                 if(x>=-128 && x<=127)System.out.println("* byte");
-                if(x >= -Math.pow(2, 15) && x <= Math.pow(2, 15))System.out.println("* short");
-                if(x >= -Math.pow(2, 31) && x <= Math.pow(2, 31))System.out.println("* int");
-                if(x >= -Math.pow(2, 63) && x <= Math.pow(2, 63))System.out.println("* long");
+                if(x >= -Math.pow(2, 15) && x <= Math.pow(2, 15)-1)System.out.println("* short");
+                if(x >= -Math.pow(2, 31) && x <= Math.pow(2, 31)-1)System.out.println("* int");
+                if(x >= -Math.pow(2, 63) && x <= Math.pow(2, 63)-1)System.out.println("* long");
             }
             catch(Exception e)
             {
